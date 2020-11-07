2a25b27cadcfc6c71d80d715f376d6bb48ab1bce
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-11-06 21:55:30 -0800

    Update ENGR 467 Project.py

--- ENGR 467 Project.py
+++ ENGR 467 Project.py
@@ -8,51 +8,53 @@ class Draw(Frame):
         self.initUI()
 
     def initUI(self):
-        #This is where the Schedule is Drawn
+        #This is where the Schedule base is Drawn
 
-        self.master.title("Schedule")
         self.grid()
         Counter = 0
         self.canvas = Canvas(self,width=1000, height=300)
+        
         for i in range(10,220,30):                  #Draws the Initial X-Axis Lines
             self.canvas.create_line(10, i, 1000, i) #Format(x1,y1,x2,y2)
+            
         for i1 in range(10,1000,30):                #Draws the Y-Axis Lines
             self.canvas.create_line(i1, 220, i1, 230)
-            self.canvas.create_text(i1,240,fill="darkblue",font="Times 12 italic bold",text=str(Counter)) #Draws the numbers
+            self.canvas.create_text(i1,240,fill="darkblue",font="Times 12 italic bold",text=str(Counter))
             Counter+=1
 
         self.canvas.grid()
         self.Draw_Task(5,0,3)
-        self.Draw_Task(2,9,13)
+        self.Draw_Task(3,0,13)
+        
     def Draw_Task(self,Task,Begin,End):
         self.canvas.create_rectangle((30*(Begin)+10), ((Task*30)+10), (30*(End)+10), ((Task*30)+40),fill="blue")
         
-    def Logic(self):
+    def Logic(self): #Logic Module, This will decide which tasks to draw and notify of deadline misses
         #(8, 1), (15, 3), (20, 4), and (22, 6) test
         #Release, Period, and Execution Time
         self.update()
-        Total_Task=[]
         Task0 = "0,0,70"     #self.txtin0.get()
         Task1 = "5,0,40"    #self.txtin1.get()
         Task2 = "10,0,35"    #self.txtin2.get()
         Task3 = "0,0,0"    #self.txtin3.get()
         Task4 = "0,0,0"     #self.txtin4.get()
         #TimeQuantum=20      #self.txtin5.get()
+        
         Task0 = Task0.split(",")
         Task1 = Task1.split(",")
         Task2 = Task2.split(",")
         Task3 = Task3.split(",")
         Task4 = Task4.split(",")
+        
         Release=[int(Task0[0]),int(Task1[0]),int(Task2[0]),int(Task3[0]),int(Task4[0])]
         Period=[int(Task0[1]),int(Task1[1]),int(Task2[1]),int(Task3[1]),int(Task4[1])]
         Execution=[int(Task0[2]),int(Task1[2]),int(Task2[2]),int(Task3[2]),int(Task4[2])]
+        
         for i in range(0,len(Release)-1):
-            #print(i)
             Width=Release[i+1]-Release[i]
-            #print(Release[i])
             print(Width)
 
-class App(Tk): 
+class App(Tk): #This Module sets up the original window with search boxes, labels, and a button
 
     def __init__(self, *args, **kwargs):
 
@@ -78,6 +80,7 @@ class App(Tk):
         self.txt4.grid(row=4, column=0, sticky='w')
         self.txt5 = tk.Label(self, text="Task 5:",bg='light grey')
         self.txt5.grid(row=5, column=0, sticky='w')
+        
         self.txtin0 = tk.Entry(self,width=20) 
         self.txtin0.grid(row=1,column=0)
         self.txtin1 = tk.Entry(self,width=20) 
@@ -88,6 +91,7 @@ class App(Tk):
         self.txtin3.grid(row=4,column=0)
         self.txtin4 = tk.Entry(self,width=20) 
         self.txtin4.grid(row=5,column=0)
+        
         self.button0 = tk.Button(self, text="Start", command=Draw) #When Clicked, the draw class is called
         self.button0.grid(row=0,column=1,sticky='w')
 
