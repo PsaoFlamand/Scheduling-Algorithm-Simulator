4a5ed2cc1d8a65853ace87d0cfe4bd90299c85db
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-12-01 22:39:34 -0800

    Update ENGR 467 Project.py

--- ENGR 467 Project.py
+++ ENGR 467 Project.py
@@ -40,7 +40,7 @@ class Algorithms():
         print(Ut)
         return Task_List,Begin_List,End_List,deadline_missed,frequency
         
-    def fcfs (self,release,deadline,Execution): #FCFS algorithm###!!!!!!!!!!!!!!!!!!DONE
+    def fcfs (self,release,period,Execution,deadline): #FCFS algorithm###!!!!!!!!!!!!!!!!!!DONE
         Task_List=[]
         Begin_List=[]
         End_List=[]
@@ -74,8 +74,8 @@ class Algorithms():
         #print(deadline_missed)
         return Task_List,Begin_List,End_List,deadline_missed
     
-    def rr (self,release,deadline,Execution, quantum, N,context_switching): #RR algorithm###!!!!!!!!!!!!!!!!!!DONE
-        
+    def rr (self,release,period,Execution,deadline, quantum, N,context_switching): #RR algorithm###!!!!!!!!!!!!!!!!!!DONE
+        explanation=[]
         Task_List=[]
         Begin_List=[]
         End_List=[]       
@@ -89,7 +89,7 @@ class Algorithms():
             prioritized_task.append(int(priority[0]))
             prioritized_task_test.append(int(priority[0]))
             prioritized_release.append(int(priority[1]))
-        prev_start=prioritized_release[0]
+        prev_start=float(prioritized_release[0])
         exe_count=0
         task_num=prioritized_task[0]
         del prioritized_task[0]
@@ -104,6 +104,7 @@ class Algorithms():
                 if (end)>deadline[prioritized_task_test[exe_count]] and reset==0:#detect missed deadline
                     reset=1
                     deadline_missed.append(deadline[prioritized_task_test[exe_count]])
+                    explanation.append("Task "+str(prioritized_task_test[exe_count])+ " Missed Its Deadline At The Time Interval: "+str(deadline[prioritized_task_test[exe_count]]))
                 if remaining_execution[exe_count]==0:
                     write=0
                     break
@@ -139,24 +140,24 @@ class Algorithms():
                         break
                     else:
                         break
-            prev_start+=context_switching
-        return Task_List,Begin_List,End_List,deadline_missed
+            prev_start+=float(context_switching)
+        return Task_List,Begin_List,End_List,deadline_missed,explanation
 
 #####################################################All Graphics and controls beyond  this point
 class Draw_Schedule(Frame):
     
-    def __init__(self,Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context):
+    def __init__(self,Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context,deadline,end_time):
         super().__init__()
         algo=Algorithms()
         if (algo_type=="eedf"):
-            Task,Begin,End,missed_deadline,frequency=algo.eedf(Release,Period,Execution,ac1,ac2)
+            Task,Begin,End,missed_deadline,frequency,explanation=algo.eedf(Release,Period,Execution,ac1,ac2)
             #Task_List,Begin_List,End_List,deadline_missed,frequency
         if (algo_type=="fcfs"):
-            Task,Begin,End,missed_deadline=algo.fcfs(Release,Period,Execution)
+            Task,Begin,End,missed_deadline,explanation=algo.fcfs(Release,Period,Execution,deadline)
         if (algo_type=="rr"):
-            Task,Begin,End,missed_deadline=algo.rr(Release,Period,Execution, quantum,N, int(context))
+            Task,Begin,End,missed_deadline,explanation=algo.rr(Release,Period,Execution,deadline, quantum,N, float(context))
         self.Draw_Structure(N)
-        self.Draw_Task(Task,Begin,End,missed_deadline)
+        self.Draw_Task(Task,Begin,End,missed_deadline,explanation,int(end_time))
 
     def Draw_Structure(self,N):##################################### Must Adjust Schedule Diagram to Number Of tasks Len(Task_Number) and change the range by Task_Number/30
         #This is where the Schedule base is Drawn 
@@ -183,15 +184,15 @@ class Draw_Schedule(Frame):
                          #Draws the Initial X-Axis Lines
         self.canvas.create_line(25, 1000, 1000, 1000) #Format(x1,y1,x2,y2)
         self.canvas.create_text(10,i-13,fill="darkblue",font="Times 12 italic bold",text="F"+str(frequency))
-            tsknum+=1
-        for freq in frequency:
+        #tsknum+=1
+        #for freq in frequency:
             
-        self.canvas.grid()
-        
-    def Draw_Task(self,Task_List,Begin_List,End_List,missed_deadline):
+        #self.canvas.grid()
+        #
+    def Draw_Task(self,Task_List,Begin_List,End_List,missed_deadline,explanation,end_time):
         scale=1
         Counter = 0
-        mx=End_List[len(End_List)-1]
+        mx=end_time#End_List[len(End_List)-1]
         if (mx>320):
             scale=20
         if(mx>160 and mx <=320):
@@ -206,14 +207,29 @@ class Draw_Schedule(Frame):
             self.canvas.create_line(i1, (((N+1)*30)-20), i1, (((N+1)*30)-10))
             self.canvas.create_text(i1,(((N+1)*30)),fill="darkblue",font="Times 12 italic bold",text=str(Counter))
             Counter+=scale
+            
         for i in range(0,len(Task_List)):
             Task=Task_List[i]
             Begin=Begin_List[i]/scale
             End=End_List[i]/scale
             self.canvas.create_rectangle((30*(Begin)+25), ((Task*30)+10), (30*(End)+25), ((Task*30)+40),fill="blue")
-            for dead in missed_deadline:
-                self.canvas.create_line((30*(dead/scale)+25), (10), (30*(dead/scale)+25), (140),fill="red")
-
+            count=0
+        for dead in missed_deadline:
+            self.canvas.create_line((30*(dead/scale)+25), (10), (30*(dead/scale)+25), (((N+1)*26)),fill="red",width=5)
+        count=float(1)
+        count_for_break=0
+        if len(explanation)==0:
+            self.canvas.create_text(200,(((N+1)*40)*count),fill="darkblue",font="Times 10 italic bold",text="All Tasks Were Schedulable!")
+        else:
+            for exp in explanation:
+                print(exp)
+                self.canvas.create_text(200,(((N+1)*40)*count),fill="darkblue",font="Times 10 italic bold",text=str(exp))
+                count+=float(0.1)
+                print(N)
+                if count_for_break==N-1:
+                    break
+                count_for_break+=1
+            
 class Main(Tk): #This Module sets up the original window with search boxes, labels, and a button
     
     def __init__(self, *args, **kwargs):
@@ -255,18 +271,21 @@ class Main(Tk): #This Module sets up the original window with search boxes, labe
         
         self.quantum_get=tk.Entry(self,width=10)
         self.context_get=tk.Entry(self,width=10)
+        self.end_time_get=tk.Entry(self,width=10)
         self.quantum_text = tk.Label(self, text="Quantum",bg='yellow',font=self.task_text)
         self.context_text = tk.Label(self, text="Context",bg='yellow',font=self.task_text)
+        self.end_time_text = tk.Label(self, text="End Time",bg='yellow',font=self.task_text)
         self.quantum_get.grid(row=7,column=1, sticky='e')
         self.quantum_text.grid(row=7, column=1, sticky='w')
         self.context_get.grid(row=8,column=1, sticky='e')
         self.context_text.grid(row=8,column=1, sticky='w')
+        self.end_time_get.grid(row=9,column=1, sticky='e')
+        self.end_time_text.grid(row=9,column=1, sticky='w')
         
         ##Explanation of input
-        self.explainEEDF = tk.Label(self, text="|Release,Period,Execution| or",bg='yellow',font=self.explain_text)
-        self.explainEEDF.grid(row=9,column=1, sticky='w')
-        self.explainEEDF = tk.Label(self, text="|Release,Deadline,Execution|",bg='yellow',font=self.explain_text)
+        self.explainEEDF = tk.Label(self, text="|Release,Period,Exe,Dead|",bg='yellow',font=self.explain_text)
         self.explainEEDF.grid(row=10,column=1, sticky='w')
+
     def Add_Task(self):
         global counter
         counter +=1
@@ -288,23 +307,28 @@ class Main(Tk): #This Module sets up the original window with search boxes, labe
         Release={}
         Period={}
         Execution={}
+        deadline={}
         ac1={}
         ac2={}
         N=len(entry_list)
         if (var1.get() == 1):#EEDF
-            entry_list_test=["0,8,3,2,1","0,10,3,1,1","0,14,1,1,1"]
+            entry_list_test=["0,8,3,2,1,0","0,10,3,1,1,0","0,14,1,1,1,0"]#added deadline as the last bit
             for i in entry_list_test:
                 Task=i#.get()
                 Task = Task.split(",")
                 Release.update({count:int(Task[0])})
                 Period.update({count:int(Task[1])})
                 Execution.update({count:int(Task[2])})
+                
                 ac1.update({count: int(Task[3])})
                 ac2.update({count: int(Task[4])})
+                deadline.update({count:int(Task[5])})
                 count+=1
             algo_type="eedf"
             quantum=0
-            Draw_Schedule(Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context)
+            context=0#self.context_get.get()
+            end_time=self.end_time_get.get()
+            Draw_Schedule(Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context,deadline,end_time)
         elif (var2.get() == 1):###FCFS#######################################
             entry_list_test=["10,100,20","5,60,20","20,20,15","30,100,15"] #(release,deadline,execution)
             for i in entry_list_test:
@@ -313,25 +337,29 @@ class Main(Tk): #This Module sets up the original window with search boxes, labe
                 Release.update({count:int(Task[0])})
                 Period.update({count:int(Task[1])})
                 Execution.update({count:int(Task[2])})
+                deadline.update({count:int(Task[3])})
                 count+=1
             algo_type="fcfs"
             quantum=0
             context=0#self.context_get.get()
-            Draw_Schedule(Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context)
+            end_time=self.end_time_get.get()
+            Draw_Schedule(Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context,deadline,end_time)
         elif (var3.get() == 1):###RR
-            #entry_list_test=["30,60,20","20,70,20","10,80,15","5,90,15"]
-            entry_list_test=["0,100,75","10,100,40","10,100,25","80,100,20","85,100,45"]
+            #entry_list_test=["30,0,20,60","20,0,20,70","10,0,15,80","5,0,15,90"]
+            entry_list_test=["0,0,75,300","10,0,40,500","10,0,25,700","80,0,20,900","85,0,45,1010"]
             for i in entry_list_test:
                 Task=i#.get()
                 Task = Task.split(",")
                 Release.update({count:int(Task[0])})
                 Period.update({count:int(Task[1])})
                 Execution.update({count:int(Task[2])})
+                deadline.update({count:int(Task[3])})
                 count+=1
             algo_type="rr"
             quantum=15#self.quantum_get.get()
             context=self.context_get.get()
-            Draw_Schedule(Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context)
+            end_time=self.end_time_get.get()
+            Draw_Schedule(Release,Period,Execution,N,algo_type,quantum,ac1,ac2,context,deadline,end_time)
         self.clear()
         N=0
         
